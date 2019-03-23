7480996b4419d78346d1b4a39277a4561bf40137
AkatsukiCC <37166471+AkatsukiCC@users.noreply.github.com>
2019-03-23 20:00:18 +0800

    Update simulator0.py

--- simulator0.py
+++ simulator0.py
@@ -551,7 +551,9 @@ class simulation(object):
                 if cross.__update__() or cross.__done__():
                     self.dead = False
             unfinishedCross = nextCross
-            assert self.dead is False, print("dead lock in", unfinishedCross)
+            if self.dead：
+                print("dead lock in", unfinishedCross)
+                return
         print("car pulling away from carport")
         for i in range(CROSSNAMESPACE.__len__()):
             crossId = CROSSNAMESPACE[i]
